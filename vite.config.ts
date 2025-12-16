e54a45a279054b71cdda98b3d2f2a9fdb5c49559
andrew linn <andrewlinn957@gmail.com>
2025-12-16 09:36:46 +0000

    Added github pages support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/banksim/' : '/',
   plugins: [react()],
   test: {
     globals: true,
@@ -9,4 +10,4 @@ export default defineConfig({
     setupFiles: [],
     includeSource: ['src/engine/simulationTestSuite.ts'],
   },
-});
+}));
